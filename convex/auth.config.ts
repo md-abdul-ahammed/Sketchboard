06291e24a244f787bfa3c553da7b852f78c04b37
md-abdul-ahammed <mdabdulahammed1@gmail.com>
2024-02-13 21:31:52 +0600

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  providers: [
+    {
+      domain: "https://generous-javelin-10.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
